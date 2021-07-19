36f641ada965d5d66d2eb88204f8f42430016838
edsaac <esaavedrac@u.northwestern.edu>
2021-07-19 10:35:57 -0500

    tested local machine

--- subsurfaceFiltration/runCases/usingPressureFT-LLS/newPressureSignal/AllRun.sh
+++ subsurfaceFiltration/runCases/usingPressureFT-LLS/newPressureSignal/AllRun.sh
@@ -1,7 +1,8 @@
+NPROC=$1
 decomposePar
-mpirun -np 24 filtrationFix -parallel > log 
+mpirun -np $NPROC filtrationFix -parallel > log 
 reconstructPar
 rm -r processor*
-foamToVTK
+foamToVTK -noZero
 #foamListTimes -rm
 
